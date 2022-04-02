c05d7876a534d18ec53903fa22386a731edb337b
Maxim <demuramax@gmail.com>
2022-04-02 21:07:32 +0300

    edit

--- Scrapy/scrapy_learn.py
+++ Scrapy/scrapy_learn.py
@@ -8,10 +8,15 @@ class WorldmetersSpider(scrapy.Spider):
     
     def parse(self, response):
         title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a/text()').getall()
+        countries = response.xpath('//td/a').getall()
+        
+        
+        for country in countries: 
+            country_name = country.xpath('.//text()').get()
+            link = country.xpath('.//@href)').get()
         
         yield {
-            'title': title,
-            'countries': countries
+            'country_name': country_name,
+            'link': link
         }
         
