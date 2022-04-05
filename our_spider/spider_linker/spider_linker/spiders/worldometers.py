db77c480a2db7797ce788b509f19490a3fe5f1ae
Maxim <demuramax@gmail.com>
2022-04-05 09:20:06 +0300

    country spider

--- our_spider/spider_linker/spider_linker/spiders/worldometers.py
+++ our_spider/spider_linker/spider_linker/spiders/worldometers.py
@@ -6,6 +6,7 @@ class WorldometersSpider(scrapy.Spider):
     allowed_domains = ['www.worldometers.info']
     start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
 
+    
     def parse(self, response):
         title = response.xpath('//h1/text()').get()
         countries = response.xpath('//td/a/text()').getall()
@@ -15,7 +16,23 @@ class WorldometersSpider(scrapy.Spider):
             country_name = country.xpath('.//text()').get()
             link = country.xpath('.//@href').get()
         
+            #absolute url
+            # absolute_url = f'https://www.worldometers.info/{link}'
+            # absolute_url = response.urljoin(link)
+            
+            #relative url
+            yield response.follow(url=link, callback=self.parse_country, meta={'country': country_name})
+    
+    def parse_country(self, response):
+    #response.xpath('(//table[@class="table table-striped table-bordered table-hover table-condensed table-list"])[1]/tbody/tr')
+        country = response.request.meta['country']
+        rows = response.xpath('(//table[contains(@class, "table")])[1]/tbody/tr')
+        for row in rows:
+            year = row.xpath('.//td[1]/text()').get()
+            population = row.xpath('.//td[2]/strong/text()').get()
+            
             yield {
-                'country_name': country_name,
-                'link': link
+                'country': country,
+                'year': year,
+                'population': population,
             }
